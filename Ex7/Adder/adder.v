5a0444dbf4ef23f4035bbce7da7a11ccc29730c9
Mohammad Sharifi Sadeghi <m.sharifisadeghi@khu.ac.ir>
2021-12-22 17:18:15 +0330

    remove riscv segments from Ex7

--- Ex7/Adder/adder.v
+++
@@ -1,5 +0,0 @@
-module add(addr, const, out);
-    output [31:0] out;
-    input [31:0] addr, const;
-    assign out = const + addr;
-endmodule
--- Ex7/InstructionMemory/instructionMemory.v
+++
@@ -1,7 +0,0 @@
-module instructionMemory(addr, out);
-   input [5:0] addr;
-   output [31:0] out;
-   reg [31:0] ram[0:63];
-   initial $readmemh("ram.dat", ram); // create virtual ram
-   assign out = ram[addr];
-endmodule
